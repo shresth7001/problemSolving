0d9c4630d2adb137b3a44bf5382566331ac56edd
shresth7001 <42043261+shresth7001@users.noreply.github.com>
2021-07-22 10:25:36 +0530

    Add files via upload

---
+++ Build an Array With Stack Operations.java
@@ -0,0 +1,39 @@
+/* 
+Given an array target and an integer n. In each iteration, you will read a number from  list = {1,2,3..., n}.
+
+Build the target array using the following operations:
+
+Push: Read a new element from the beginning list, and push it in the array.
+Pop: delete the last element of the array.
+If the target array is already built, stop reading more elements.
+Return the operations to build the target array. You are guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: target = [1,3], n = 3
+Output: ["Push","Push","Pop","Push"]
+Explanation: 
+Read number 1 and automatically push in the array -> [1]
+Read number 2 and automatically push in the array then Pop it -> [1]
+Read number 3 and automatically push in the array -> [1,3]
+
+*/
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> list = new ArrayList();
+        int input =1;
+        for(int i : target){
+            while(input<i){
+                list.add("Push");
+                list.add("Pop");
+            input++;
+            }
+            list.add("Push");
+            input++;
+                }
+ 
+    return list;
+    }
+}
